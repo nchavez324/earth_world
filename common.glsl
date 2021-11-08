b24815d714bddbe09bdba9c6b07166d482631ce5
Nick Chavez <chavez.a.nicolas@gmail.com>
2021-11-08 17:01:53 -0500

    Use spherical distance for vision range.

--- common.glsl
+++ common.glsl
@@ -9,8 +9,8 @@
 #define BATHYMETRY_CUTOFF 0.83
 
 // The fade for sight visibility around the boat.
-#define VISIBILITY_FADE_START 0.04
-#define VISIBILITY_FADE_END 0.05
+#define VISIBILITY_FADE_START 0.07
+#define VISIBILITY_FADE_END 0.1
 
 vec3 sphericalCoordsFromCartesian(vec3 coords) {
   // Panda3D is right-hand z-up, and world coords are still in Panda coords
@@ -32,6 +32,26 @@ vec2 sphericalUVFromCartesian(vec3 coords) {
   return vec2(
     sphericalCoords.x / TWO_PI,
     (sphericalCoords.y + PI_OVER_TWO) / PI);
+}
+
+vec3 cartesianCoordsFromSpherical(vec3 coords) {
+  // x is azimuth [0,2PI], y is polar [-PI/2,PI/2], z is radius.
+  return vec3(
+    coords.z * cos(coords.y) * cos(coords.x),
+    coords.z * cos(coords.y) * sin(coords.x),
+    coords.z * sin(coords.y));
+}
+
+/** This assumes both are normalized, radius 1. */
+float unitSphereDistance(vec2 s1, vec2 s2) {
+  // Find the angle between the two, when in cartesian.
+  vec3 v1 = cartesianCoordsFromSpherical(vec3(s1, 1));
+  vec3 v2 = cartesianCoordsFromSpherical(vec3(s2, 1));
+  float dotProduct = dot(v1, v2);
+  // acos starts going haywire when the vectors are really close together.
+  // TODO: estimate a better metric than this.
+  if (dotProduct >= 0.99999) return 0;
+  return acos(dotProduct);
 }
 
 float inverseMix(float from, float to, float value) {
